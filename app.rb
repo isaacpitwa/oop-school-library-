0baad8e696137c1bc0eec3b50171b89a0ad71a43
isaacpitwa <isaacpitwa256@gmail.com>
2022-06-17 21:23:03 +0300

    Add selection for User Input

--- app.rb
+++ app.rb
@@ -11,7 +11,19 @@ class App
     @books = []
     @rentals = []
   end
-  
+
+  def selection(input)
+    case input
+    when 1 then list_books
+    when 2 then list_persons
+    when 3 then create_person
+    when 4 then create_book
+    when 5 then create_rental
+    when 6 then list_rentals
+    when 7 then exit
+    end
+  end
+
   def menu
     puts [
       '1 - List all books',
