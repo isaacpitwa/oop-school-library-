ca3b7c7c6220fc5233765b233333e9de4d1ee97b
isaacpitwa <isaacpitwa256@gmail.com>
2022-06-17 21:21:36 +0300

    Add App.rb

---
+++ app.rb
@@ -0,0 +1,15 @@
+require './person'
+require './student'
+require './teacher'
+require './classroom'
+require './book'
+require './rental'
+
+class App
+  def initialize
+    @persons = []
+    @books = []
+    @rentals = []
+  end
+
+end
