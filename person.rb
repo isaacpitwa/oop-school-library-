a94824ca8013cdad08ed29de5982cda3f6def53a
isaacpitwa <isaacpitwa256@gmail.com>
2022-06-17 03:03:19 +0300

    ADD Relationships of retals to book, person

--- person.rb
+++ person.rb
@@ -6,7 +6,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
-
+    @rentals = []
     super()
   end
 
@@ -23,6 +23,6 @@ class Person < Nameable
   end
 
   private :of_age
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 end
--- student.rb
+++ student.rb
@@ -6,7 +6,7 @@ class Student < Person
   def initialize(parent_permission, age,classroom, classroom, name = 'Unknown')
     super(age, name, parent_permission)
     @classroom = classroom
-    @classroom.add_student(self)
+    @classroom.add_student(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
